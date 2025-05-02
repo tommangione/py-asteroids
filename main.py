c00a7695b11832016a91dccadb81a2f9fd2caef7
tommangione <tommangione@pm.me>
2025-05-02 08:39:06 -0400

    guess we drawin' rectangles now

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from constants import *
+import pygame
 
 def main():
+    pygame.init()
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while (0<1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black", rect=None, special_flags=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()

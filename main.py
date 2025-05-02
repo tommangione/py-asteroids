15f72b94d75a5aac31db9cf877e8f56f8eeab6f4
tommangione <tommangione@pm.me>
2025-05-02 11:42:56 -0400

    fixed resource use

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()

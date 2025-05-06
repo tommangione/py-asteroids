9759711f14c3febd10677f507407a06665f2ace5
tommangione <tommangione@pm.me>
2025-05-05 21:48:18 -0400

    now we a triangle

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from constants import *
+from player import *
 import pygame
+
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     clock = pygame.time.Clock()
     dt = 0
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while (0<1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
+        for member in updatable:
+            member.update(dt)
+        for member in drawable:
+            member.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
